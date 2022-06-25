5f68805c0794d508ce9a30f3508410d994edff9e
Tuanemendes <tuane.belmiro@gmail.com>
2022-06-24 22:12:38 -0300

    aplicando cache na pagina home

--- mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -27,6 +27,7 @@ public class HomeController {
 		
 		Sort sort = Sort.by("dataDaEntrega").descending();
 		PageRequest paginacao = PageRequest.of(0, 10,sort);
+		
 		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE,paginacao);
 	    model.addAttribute("pedidos",pedidos);
 		return "home";
